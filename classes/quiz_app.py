1cc0f432011410cb5b7886a32a1b840f5515a5fd
ashescanilla <joannaashleyescanilla@gmail.com>
2025-05-25 22:41:37 +0800

    QuizApp: class initialization and colorama initialiazation

--- classes/quiz_app.py
+++ classes/quiz_app.py
@@ -4,3 +4,11 @@ import os
 import sys
 from colorama import init, Fore, Style
 import time
+
+init(autoreset=True)
+
+class QuizApp:
+    def __init__(self, file_name = "quiz_data.txt"):
+        self.file_name = file_name
+        self.creator = QuizCreator(self.file_name)
+        self.reader = QuizReader(self.file_name)
